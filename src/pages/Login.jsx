af926ea515d753318c12c08bd617416796415a0a
praveenKUMAR-05 <150874893+praveenKUMAR-05@users.noreply.github.com>
2025-03-27 22:05:56 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://skill-backend-fawn.vercel.app/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
