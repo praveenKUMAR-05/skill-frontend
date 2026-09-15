f8f13841454f34908365463560ad0cb58c11ebf7
PRAVEEN KUMAR MOHAN <150874893+praveenKUMAR-05@users.noreply.github.com>
2026-09-15 10:11:24 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await ffetch("https://skill-backend-b3n9.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
